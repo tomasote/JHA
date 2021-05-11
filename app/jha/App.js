aab5bc931a29093186f7f542fd70cf9e0167e293
Tomas Pérez <poatati@hotmail.com>
2021-05-11 23:24:09 +0200

    GRADIENTS!!!

--- app/jha/App.js
+++ app/jha/App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#4c669f', '#ffffff']}
+        style={styles.background}
+      />
       <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+let ScreenHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +25,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    position: 'absolute',
+    flex: 1,
+    left: 0,
+    right: 0,
+    top: 0,
+    height: ScreenHeight,
   },
 });
